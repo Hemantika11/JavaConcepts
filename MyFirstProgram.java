e3abf7e0746cbdad6a2a72c8fc4d7f89713bf54f
Hemantika <hemarpatel@yahoo.co.uk>
2022-07-19 20:56:20 +0100

    my first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+package JavaConcepts;
+
+public class MyFirstProgram
+{
+   public static void main(String[] args)
+   {
+       System.out.println("First Java Application");
+       System.out.println("Hemantika");
+   }
+}
