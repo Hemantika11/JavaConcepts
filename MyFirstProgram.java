279a8355e2654911629754c1ee66fb42cf3dabd1
Hemantika <hemarpatel@yahoo.co.uk>
2022-07-19 21:08:17 +0100

    my first name Hemantika Patel

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -5,6 +5,6 @@ public class MyFirstProgram
    public static void main(String[] args)
    {
        System.out.println("First Java Application");
-       System.out.println("Hemantika");
+       System.out.println("Hemantika Patel");
    }
 }
